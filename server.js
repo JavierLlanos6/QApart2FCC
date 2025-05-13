d35fa97801700cef7ae59aef00b588a7f1872b8f
JavierLlanos6 <xavi6poop@gmail.com>
2025-05-13 16:01:32 -0400

    Finish until exercise 4

--- server.js
+++ server.js
@@ -1,21 +1,71 @@
-'use strict';
-require('dotenv').config();
-const express = require('express');
-const myDB = require('./connection');
-const fccTesting = require('./freeCodeCamp/fcctesting.js');
+"use strict";
+require("dotenv").config();
+const express = require("express");
+const session = require("express-session");
+const passport = require("passport");
+const { ObjectID } = require("mongodb");
+const myDB = require("./connection");
+const fccTesting = require("./freeCodeCamp/fcctesting.js");
 
 const app = express();
 
-fccTesting(app); //For FCC testing purposes
-app.use('/public', express.static(process.cwd() + '/public'));
+// Configuración del motor de plantillas
+app.set("view engine", "pug");
+app.set("views", "./views/pug");
+
+// Middlewares generales
+fccTesting(app);
+app.use("/public", express.static(process.cwd() + "/public"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.route('/').get((req, res) => {
+// Configurar sesión
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET,
+    resave: true,
+    saveUninitialized: true,
+    cookie: { secure: false },
+  })
+);
 
+app.use(passport.initialize());
+app.use(passport.session());
+
+// CONEXIÓN A LA BASE DE DATOS Y SERIALIZACIÓN
+myDB(async (client) => {
+  const myDataBase = await client.db("database").collection("users");
+
+  // SERIALIZACIÓN Y DESERIALIZACIÓN
+  passport.serializeUser((user, done) => {
+    done(null, user._id);
+  });
+
+  passport.deserializeUser((id, done) => {
+    myDataBase.findOne({ _id: new ObjectID(id) }, (err, doc) => {
+      done(err, doc);
+    });
+  });
+
+  // RUTA PRINCIPAL
+  app.route("/").get((req, res) => {
+    res.render("index", {
+      title: "Connected to Database",
+      message: "Please login",
+    });
+  });
+}).catch((e) => {
+  // SI FALLA LA CONEXIÓN
+  app.route("/").get((req, res) => {
+    res.render("index", {
+      title: e,
+      message: "Unable to connect to database",
+    });
+  });
 });
 
+// LISTEN FUERA DE myDB
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log('Listening on port ' + PORT);
+  console.log("Listening on port " + PORT);
 });
